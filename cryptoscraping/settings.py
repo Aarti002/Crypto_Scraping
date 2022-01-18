f9a12739a53d6956474a04f1eced0bfb0669e044
Aarti singh <aartikumarisingh120@gmail.com>
2022-01-18 11:28:17 +0530

    heroku-update-2

--- cryptoscraping/settings.py
+++ cryptoscraping/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b*(dy$t7gcf!b_#bienepepp_(jwcvl)xl2it8k=w5^mo%v8qc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
